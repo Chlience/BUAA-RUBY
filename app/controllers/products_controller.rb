812b7b09e42af4390275887292bf14b7437a958e
chlience <chlience@outlook.com>
2023-12-20 20:54:39 +0800

    fix some css err

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,7 @@ class ProductsController < ApplicationController
 
   # GET /products/1/edit
   def edit
+    @new_product_type = ProductType.new
   end
 
   # POST /products or /products.json
@@ -30,34 +31,26 @@ class ProductsController < ApplicationController
       @product.product_url = "https://gw.alicdn.com/bao/uploaded/i1/2508474491/O1CN014jg2pU1j2vcMOqiXq_!!2508474491-0-picasso.jpg_300x300q90.jpg_.webp"
     end
 
-    respond_to do |format|
-      if @product.save
-        @product_type = ProductType.new(product_type_name: "默认", product_id: @product.id, price: 0)
-        @product_type.save!
-        @product_type_size = ProductTypeSize.new(product_type: @product_type, size: "默认")
-        @product_type_size.save!
-        @product_type_color = ProductTypeColor.new(product_type: @product_type, color: "默认")
-        @product_type_color.save!
+    if @product.save
+      @product_type = ProductType.new(product_type_name: "默认", product_id: @product.id, price: 0)
+      @product_type.save!
+      @product_type_size = ProductTypeSize.new(product_type: @product_type, size: "默认")
+      @product_type_size.save!
+      @product_type_color = ProductTypeColor.new(product_type: @product_type, color: "默认")
+      @product_type_color.save!
 
-        format.html { redirect_to product_url(@product), notice: "商品创建成功！" }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+      redirect_back(fallback_location: root_path, notice: "商品创建成功！")
+    else
+      redirect_back(fallback_location: root_path, notice: "商品创建失败！")
     end
   end
 
   # PATCH/PUT /products/1 or /products/1.json
   def update
-    respond_to do |format|
-      if @product.update(product_params)
-        format.html { redirect_to product_url(@product), notice: "商品更新成功！" }
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.update(product_params)
+      redirect_back(fallback_location: root_path, notice: "商品更新成功！")
+    else
+      redirect_back(fallback_location: root_path, notice: "商品更新失败！")
     end
   end
 
